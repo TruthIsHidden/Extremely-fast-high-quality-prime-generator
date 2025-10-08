ed95403f0b3a4b82cf0d47e6daeccd7dbe72bb0d
TruthIsHidden <xhingvapen@gmail.com>
2025-10-08 22:14:30 +0530

    the good stuff
    idk

---
+++ Main.cpp
@@ -0,0 +1,243 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <iostream>
+#include <vector>
+
+uint64_t ModPrime(uint64_t og, int irange, int erange);
+uint64_t MidPrime(uint64_t og, int irange, int erange);
+uint64_t MaxPrime(uint64_t og, int irange, int erange);
+uint64_t TheoreticalMaxPrime(uint64_t og, int irange, int erange);
+bool is_prime(uint64_t n);
+using namespace std;
+
+int counter = 0;
+int tested_counter = 0;
+
+uint64_t Poffset(uint64_t no, bool& found_prime)
+{
+    uint64_t use = no;
+    uint64_t list[18] = { 1,5,11,13,17,19,23,25,29,31,37,41,43,47,53,59,61,67 };
+    vector<uint64_t> usable;
+
+    for (int i = 0; i < 18; i++)
+    {
+        if ((use + list[i]) % 5 != 0)
+        {
+            usable.push_back(list[i]);
+            if (is_prime(use + list[i]))
+            {
+                cout << "Prime: " << (use + list[i]) << endl;
+                found_prime = true;  
+                return (use + list[i]);
+            }
+        }
+    }
+    cout << "Composite: " << (use + usable[0]) << endl;
+    found_prime = false;  
+    return use + usable[0];
+}
+
+// FIXED: Same fix for negative offset
+uint64_t Noffset(uint64_t no, bool& found_prime)
+{
+    uint64_t use = no;
+    uint64_t list[] = { 1,5,11,13,17,19,23,25,29,31,37,41,43,47,53,59,61,67 };
+    vector<uint64_t> usable;
+
+    for (int i = 0; i < 18; i++)
+    {
+        if ((use - list[i]) % 5 != 0)
+        {
+            usable.push_back(list[i]);
+            if (is_prime(use - list[i]))
+            {
+                cout << "Prime: " << (use - list[i]) << endl;
+                found_prime = true; 
+                return (use - list[i]);
+            }
+        }
+    }
+    cout << "Composite: " << (use - usable[0]) << endl;
+    found_prime = false; 
+    return use - usable[0];
+}
+bool is_prime(uint64_t n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    // Check divisibility by numbers of form 6k±1
+    for (uint64_t i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+
+uint64_t ModPrime(uint64_t og, int irange, int erange)
+{
+    int no = 0;
+    int pos, neg;
+    for (int i = irange;i < erange;i++)
+    {
+        pos = 6 * i + 13;
+        neg = 6 * i - 13;
+        if (is_prime(pos))
+        {
+            cout << "Prime: " << pos << endl;
+            no++;
+        }
+        else cout << "Composite: " << pos << endl;
+        if (is_prime(neg))
+        {
+            cout << "Prime: " << neg << endl;
+            no++;
+        }
+        else cout << "Composite: " << neg << endl;
+    }
+    return no;
+}
+uint64_t MidPrime(uint64_t og, int irange, int erange)
+{
+    int no = 0;
+    int pos, neg;
+    for (int i = irange;i < erange;i++)
+    {
+        uint64_t main = 60 * i;
+        pos = main + 1;
+        neg = main - 1;
+        if (is_prime(pos))
+        {
+            cout << "Prime: " << pos << endl;
+            no++;
+        }
+        else cout << "Composite: " << pos << endl;
+        if (is_prime(neg))
+        {
+            cout << "Prime: " << neg << endl;
+            no++;
+        }
+        else cout << "Composite: " << neg << endl;
+    }
+    return no;
+    return 0;
+
+}
+uint64_t TheoreticalMaxPrime(uint64_t og, int irange, int erange)
+{
+    int primes_found = 0;
+    int total_attempts = 0;
+
+    for (int i = irange; i < erange; i++)
+    {
+        uint64_t main = 42 * i;
+        bool pos_is_prime = false;
+        bool neg_is_prime = false;
+
+        uint64_t pos = Poffset(main, pos_is_prime);
+        uint64_t neg = Noffset(main, neg_is_prime);
+
+        if (pos_is_prime) primes_found++;
+        if (neg_is_prime) primes_found++;
+        total_attempts += 2;
+    }
+
+    cout << "\nPrimes found: " << primes_found << "/" << total_attempts;
+    if (total_attempts > 0) {
+        cout << " (" << (primes_found * 100) / total_attempts << "%)" << endl;
+    }
+
+    return primes_found;
+}
+uint64_t MaxPrime(uint64_t og, int irange, int erange)
+{
+    int no = 0;
+    int pos, neg;
+    for (int i = irange;i < erange;i++)
+    {
+        uint64_t main = 42 * i;
+        pos = ((main + 13) % 5 == 0) ? main + 11 : main + 13;
+        neg = ((main - 13) % 5 == 0) ? main - 11 : main - 13;
+        if (is_prime(pos))
+        {
+            cout << "Prime: " << pos << endl;
+            no++;
+        }
+        else cout << "Composite: " << pos << endl;
+        if (is_prime(neg))
+        {
+            cout << "Prime: " << neg << endl;
+            no++;
+        }
+        else cout << "Composite: " << neg << endl;
+    }
+    return no;
+    return 0;
+}
+int main() {
+    int choice;
+    uint64_t start, end;
+
+    while (true) {
+        cout << "\n=== PRIME GENERATOR ===" << endl;
+        cout << "1. ModPrime (6k ± 13)" << endl;
+        cout << "2. MidPrime (60k ± 1)" << endl;
+        cout << "3. MaxPrime (42k ± {11,13})" << endl;
+        cout << "4. TheoreticalMax (Dynamic 20-offset)" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Choose method: ";
+        cin >> choice;
+
+        if (choice == 5) break;
+        if (choice < 1 || choice > 4) {
+            cout << "Invalid choice!" << endl;
+            continue;
+        }
+
+        cout << "Enter start k: ";
+        cin >> start;
+        cout << "Enter end k: ";
+        cin >> end;
+
+        uint64_t primes_found, total_tested;
+
+        switch (choice) {
+        case 1:
+            primes_found = ModPrime(0, start, end);
+            total_tested = 2 * (end - start);
+            cout << "Primes: " << primes_found << "/" << total_tested;
+            cout << " (" << (primes_found * 100) / total_tested << "%)" << endl;
+            break;
+
+        case 2:
+            primes_found = MidPrime(0, start, end);
+            total_tested = 2 * (end - start);
+            cout << "Primes: " << primes_found << "/" << total_tested;
+            cout << " (" << (primes_found * 100) / total_tested << "%)" << endl;
+            break;
+
+        case 3:
+            primes_found = MaxPrime(0, start, end);
+            total_tested = 2 * (end - start);
+            cout << "Primes: " << primes_found << "/" << total_tested;
+            cout << " (" << (primes_found * 100) / total_tested << "%)" << endl;
+            break;
+
+        case 4:
+            counter = 0;
+            tested_counter = 0;
+            primes_found = TheoreticalMaxPrime(0, start, end);
+            
+            break;
+        }
+
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+    }
+
+    return 0;
+}
+
+
